40d7ab9a149b31af3fc50dc8d8fa060c3c3e8bd1
Praveenajayathissa <as20240518@sci.sjp.ac.lk>
2025-08-03 12:26:47 +0530

    initial commit - Added SUM Function

--- 1/main.c
+++ 1/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int sumofTwoNumbers(int x, int y);
+
 int main()
 {
     printf("Hello world!\n");
     printf("HELLO UNI\n");
+
     return 0;
+}
+
+int sumofTwoNumbers(int x, int y)
+{int sum;
+  sum = x+y;
+
+return sum;
 }
